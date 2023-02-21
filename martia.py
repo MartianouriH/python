d9bcb7faa44f2b02e7fb8ff1634f32141b62e23e
MartianouriH <108149715+MartianouriH@users.noreply.github.com>
2023-02-21 21:16:01 +0330

    Add files via upload

---
+++ martia.py
@@ -0,0 +1,156 @@
+name = input("what's your name ")
+if name == "martia":
+    print("ohhh " + name, "did you know that my creator's name  is martia")
+    meaning = input("what dose your name mean ")
+    print("good")
+
+
+elif name == "mahsa":
+     print("ohhh " + name, "did you know that my creator's name  is martia")
+     meaning = input("what dose your name mean ")
+
+elif name == "black hole":
+    print("A black hole is a region of spacetime where gravity is so strong that nothing, including light or other")
+    print("electromagnetic waves, has enough energy to escape its event horizon.[2] The theory of general relativity")
+    good = input("are you interestet yet? ")
+    if good == "yes":
+        print()
+        print()
+        print()
+        print()
+        if good == "no":
+            print()
+
+elif name == "می تونی فارسی صحبت کنی":
+        print("بله میتونم")
+        name = input("اسمت چیه ")
+        print(" سلام" + name)
+        meaning = input("معنی اسمت چیه ")
+        print("خوبه")
+        yes = input("اینجا یک ماشین حساب هست میخوای ازش استفاده کنی ")
+        first = float(input("شماره اول "))
+        secend = float(input("شماره دوم "))
+
+        if yes == "yes" or "y":
+            sum = first + secend
+            print("in plus: " + str(sum))
+
+        if yes == "-":
+            sum = first - secend
+            print("in dc: " + str(sum))
+
+        if yes == "x" or "*":
+            sum = first * secend
+            print("in times: " + str(sum))
+
+        if yes == "/":
+            sum = first / secend
+            print("in times: " + str(sum))
+
+
+
+else:
+    print("hello " + name)
+    meaning = input("what dose your name mean ")
+    print("good")
+
+
+
+yes = input("there is a calculator do you wish to use it? ")
+first = float(input("first-number "))
+secend = float(input("secend-number "))
+
+
+if yes == "yes" or "y":
+
+    sum = first + secend
+    print("in plus: " + str(sum))
+
+if yes == "-":
+
+    sum = first - secend
+    print("in dc: " + str(sum))
+
+if yes == "x" or "*":
+
+    sum = first * secend
+    print("in times: " + str(sum))
+
+
+if yes == "/":
+
+    sum = first / secend
+    print("in r: " + str(sum))
+
+
+
+
+
+
+
+
+
+
+
+
+
+blk = input("if you want to find out how black holes work say im ok but if you want me to reset say reset ")
+if blk == "im ok":
+    print("A black hole is a region of spacetime where gravity is so strong that nothing, including light or other")
+    print("electromagnetic waves, has enough energy to escape its event horizon.[2] The theory of general relativity")
+    good = input("are you interestet yet")
+    if good == "yes":
+        print()
+        print()
+        print()
+        print()
+
+    elif good == "no":
+         print()
+
+
+
+
+
+
+
+
+if blk == "reset":
+     name = input("what's your name ")
+     if name == "martia":
+         print("ohhh " + name, "did you know that my creator's name  is martia")
+         meaning = input("what dose your name mean ")
+     elif name == "mahsa":
+         print("ohhh " + name, "did you know that my creator's name  is martia")
+         meaning = input("what dose your name mean ")
+
+
+     else:
+         print("hello " + name)
+         meaning = input("what dose your name mean ")
+         print("good")
+
+     yes = input("there is a calculator do you wish to use it? ")
+     first = float(input("first-number "))
+     secend = float(input("secend-number "))
+
+     if yes == "yes" or "y":
+         sum = first + secend
+         print("in plus: " + str(sum))
+
+     if yes == "-":
+         sum = first - secend
+         print("in dc: " + str(sum))
+
+     if yes == "x" or "*":
+         sum = first * secend
+         print("in times: " + str(sum))
+
+     if yes == "/":
+         sum = first / secend
+         print("in times: " + str(sum))
+
+
+
+
+
